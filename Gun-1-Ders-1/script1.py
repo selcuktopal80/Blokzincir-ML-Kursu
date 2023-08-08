bbaa36250c98b578ccbf75a7ef77940aa8fd6e7c
selcuktopal80 <128636764+selcuktopal80@users.noreply.github.com>
2023-08-08 22:57:35 +0300

    Add files via upload

---
+++ Gun-1-Ders-1/script1.py
@@ -0,0 +1,12 @@
+from dizin.Modul import AritmetikIslemler
+
+# AritmetikIslemler sınıfından bir nesne oluşturalım
+islemler = AritmetikIslemler()
+
+# Toplama metoduyla toplama işlemi yapalım
+sonuc = islemler.toplama(7,9)
+print("Toplama Sonucu: ", sonuc)
+
+# Carpma metoduyla çarpma işlemi yapalım
+sonuc = islemler.carpma(8, 14)
+print("Çarpma Sonucu:", sonuc)
---
+++ Gun-1-Ders-1/script2.py
@@ -0,0 +1,4 @@
+from dizin.alt_dizin.alt_modul import merhaba_dunya
+
+# Merhaba Dünya fonksiyonunu çağıralım
+merhaba_dunya()
